f126e0fb698af2f5f67169edcbc956ae73b9281c
Tao Dong <taodong@google.com>
2019-11-01 17:00:53 -0700

    Change project name

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# text_input_widgets
+# Text Input Demo
+This demo includes examples of text manipulations using
+ the [Characters package](https://pub.dev/packages/characters).
+
+ 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:text_input_widgets/main.dart';
+import 'package:text_input_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
